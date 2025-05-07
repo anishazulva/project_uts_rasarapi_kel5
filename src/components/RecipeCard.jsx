b24705cb6f10954cc55950a97dd741325e0e197f
sitifazriyah4 <sfazriyah55@gmail.com>
2025-05-07 11:44:23 +0700

    done

---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function RecipeCard({ title, image, id }) {
+  return (
+    <div className="bg-white p-4 rounded-lg shadow-md">
+      <img src={image} alt={title} className="w-full h-48 object-cover mb-4" />
+      <h3 className="text-lg font-semibold mb-2">{title}</h3>
+      <p className="text-gray-600">Resep yang enak dan mudah dibuat di rumah.</p>
+      <Link to={/resep/${id}} className="text-blue-500 mt-4 block">
+        Lihat Resep
+      </Link>
+    </div>
+  );
+}
+
+export default RecipeCard;
